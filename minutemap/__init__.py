2bca25b113856f4b128905c616ebf8ece7c0b4a7
Sam <sgpinkus@gmail.com>
2026-03-19 18:53:04 +1100

    Fix import.

--- minutemap/__init__.py
+++ minutemap/__init__.py
@@ -1,4 +1,4 @@
-from minutemap.main import YearMinuteMap, ParseError, parse_spec
+from .main import YearMinuteMap, ParseError, parse_spec
 
 __all__ = ["YearMinuteMap", "ParseError", "parse_spec"]
-__version__ = "0.1.0"
+__version__ = "0.1.1"
